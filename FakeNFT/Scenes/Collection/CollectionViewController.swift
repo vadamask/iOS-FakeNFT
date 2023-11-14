eac5aed268a61bbf53d4d9362d3d3351d95b37bd
CodeByViktor <cool-kyl@yandex.ru>
2023-11-14 07:27:57 +0300

    Create collection view controller and collection view model "Combine" based

--- FakeNFT/Scenes/Collection/CollectionViewController.swift
+++ FakeNFT/Scenes/Collection/CollectionViewController.swift
@@ -6,10 +6,110 @@
 //
 
 import UIKit
+import Combine
+import SnapKit
 
-final class CollectionViewController: UIViewController {
+final class CollectionViewController: UICollectionViewController, LoadingView, ErrorView {
+    enum Section {
+        case main
+    }
+    internal lazy var activityIndicator = UIActivityIndicatorView()
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, CollectionCellViewModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, CollectionCellViewModel>
+    private lazy var dataSource = makeDataSource()
+    private var subscriptions = Set<AnyCancellable>()
+    private let viewModel: CollectionViewModelProtocol
+
+    init(viewModel: CollectionViewModelProtocol, layout: UICollectionViewLayout) {
+        self.viewModel = viewModel
+        super.init(collectionViewLayout: layout)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        navigationItem.standardAppearance = appearance
+
+        collectionView.refreshControl = UIRefreshControl()
+        collectionView.backgroundColor = .screenBackground
+        collectionView.backgroundView = activityIndicator
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.registerHeaderView(CollectionHeader.self)
+        collectionView.register(CollectionCell.self)
+        collectionView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+
+        bind()
+
+        viewModel.loadCollection()
+    }
+
+    private func bind() {
+        viewModel.statePublisher.sink { [weak self] state in
+            switch state {
+            case .loading:
+                self?.showLoading()
+            case .loaded:
+                self?.hideLoading()
+                self?.applySnapshot()
+            case .error(let error):
+                self?.showError(
+                    ErrorModel(message: error.localizedDescription, actionText: "Повторить") {
+                        self?.viewModel.loadCollection()
+                    }
+                )
+            }
+        }
+        .store(in: &subscriptions)
+    }
+
+    private func makeDataSource() -> DataSource {
+        let dataSource = DataSource(
+            collectionView: collectionView
+        ) { collectionView, indexPath, collectionCellViewModel -> UICollectionViewCell? in
+            let cell: CollectionCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+            cell.viewModel = collectionCellViewModel
+            return cell
+        }
+        dataSource.supplementaryViewProvider = { [weak self] collectionView, kind, indexPath in
+            let headerView: CollectionHeader = collectionView.dequeueReusableView(
+                ofKind: kind, indexPath: indexPath
+            )
+            headerView.onNameAuthorLabelClicked = { authorUrlString in
+                let webViewVC = WebViewController(url: authorUrlString)
+                self?.navigationController?.pushViewController(webViewVC, animated: true)
+            }
+            headerView.viewModel = self?.viewModel.headerViewModel
+            return headerView
+        }
+        return dataSource
+    }
+
+    func applySnapshot() {
+        guard
+            viewModel.headerViewModel != nil,
+            let cellViewModels = viewModel.cellViewModels
+        else {
+            return
+        }
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(cellViewModels)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+
+    deinit {
+        for subscription in subscriptions {
+            subscription.cancel()
+        }
     }
 }
---
+++ FakeNFT/Scenes/Collection/CollectionViewModel.swift
@@ -0,0 +1,85 @@
+//
+//  CollectionViewModel.swift
+//  FakeNFT
+//
+//  Created by Виктор on 12.11.2023.
+//
+
+import Foundation
+import Combine
+
+enum CollectionViewState {
+    case loading, loaded, error(Error)
+}
+
+protocol CollectionViewModelProtocol {
+    var state: CollectionViewState { get }
+    var statePublisher: Published<CollectionViewState>.Publisher { get }
+    var cellViewModels: [CollectionCellViewModel]? { get }
+    var headerViewModel: CollectionHeaderViewModel? { get }
+    func loadCollection()
+}
+
+final class CollectionViewModel: CollectionViewModelProtocol {
+    private var subscriptions = Set<AnyCancellable>()
+    private let collectionId: String
+    private let service: NftService
+    private(set) var cellViewModels: [CollectionCellViewModel]?
+    private(set) var headerViewModel: CollectionHeaderViewModel?
+    @Published private(set) var state: CollectionViewState = .loading
+    var statePublisher: Published<CollectionViewState>.Publisher { $state }
+
+    init(collectionId: String, service: NftService) {
+        self.collectionId = collectionId
+        self.service = service
+    }
+
+    func loadCollection() {
+        service.loadCollection(by: collectionId)
+            .receive(on: RunLoop.main)
+            .flatMap { [unowned self] collection in
+                let user = self.service.loadUser(by: collection.author)
+                let profile = self.service.loadProfile()
+                let order = self.service.loadOrder(by: "1")
+                let nfts = collection.nfts.map { self.service.loadNft(by: $0) }
+                let nftsMerge = Publishers.MergeMany(nfts).collect().eraseToAnyPublisher()
+                return Publishers.Zip4(user, nftsMerge, order, profile)
+                    .map { nftUser, nfts, order, profile in
+                        let headerVM = CollectionHeaderViewModel(
+                            name: collection.name,
+                            author: nftUser.name,
+                            description: collection.description,
+                            cover: collection.cover,
+                            webSite: nftUser.website
+                        )
+                        let cellVM = nfts.map {
+                            CollectionCellViewModel(
+                                imageUrls: $0.images,
+                                isLiked: profile.likes.contains($0.id),
+                                name: $0.name,
+                                rating: $0.rating,
+                                price: $0.price,
+                                inOrder: order.nfts.contains($0.id))
+                        }
+                        return (headerVM, cellVM)
+                    }
+                    .eraseToAnyPublisher()
+            }
+            .sink { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.state = .error(error)
+                }
+            } receiveValue: { [weak self] headerVM, cellVM in
+                self?.headerViewModel = headerVM
+                self?.cellViewModels = cellVM
+                self?.state = .loaded
+            }
+            .store(in: &subscriptions)
+    }
+
+    deinit {
+        for subscription in subscriptions {
+            subscription.cancel()
+        }
+    }
+}
