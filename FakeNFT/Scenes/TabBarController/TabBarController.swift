0baa04d1867ed804f0e53caea1e694e47da5aed4
CodeByViktor <cool-kyl@yandex.ru>
2023-11-14 07:15:39 +0300

    Make serviceAssemble singleton

--- FakeNFT/Scenes/TabBarController/TabBarController.swift
+++ FakeNFT/Scenes/TabBarController/TabBarController.swift
@@ -1,21 +1,12 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-    private var servicesAssembly: ServicesAssembly
-
     private let catalogTabBarItem = UITabBarItem(
         title: L10n.Tab.catalog,
         image: Asset.catalog.image,
         tag: 0
     )
 
-    init(servicesAssembly: ServicesAssembly) {
-        self.servicesAssembly = servicesAssembly
-        super.init(nibName: nil, bundle: nil)
-    }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,9 +14,10 @@ final class TabBarController: UITabBarController {
         tabBar.tintColor = .tabActive
 
         // Catalog
-        let catalogViewModel = CatalogViewModel(service: servicesAssembly.nftService)
+        let catalogViewModel = CatalogViewModel(service: ServicesAssembly.shared.nftService)
         let catalogViewController = CatalogViewController(
-            viewModel: catalogViewModel
+            viewModel: catalogViewModel,
+            layout: CatalogLayout()
         )
         catalogViewController.tabBarItem = catalogTabBarItem
         let catalogNavigationController = UINavigationController(rootViewController: catalogViewController)
