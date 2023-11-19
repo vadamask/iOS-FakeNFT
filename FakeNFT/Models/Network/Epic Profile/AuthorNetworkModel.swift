5f0fdcc3cb7f5e0f2003f541e4661b25f0e18d76
Anna Goncharova <bkljn4g@gmail.com>
2023-11-19 18:24:59 +0300

    add network model for epic Profile

---
+++ FakeNFT/Models/Network/Epic Profile/AuthorNetworkModel.swift
@@ -0,0 +1,13 @@
+//
+//  AuthorNetworkModel.swift
+//  FakeNFT
+//
+//  Created by Ann Goncharova on 19.11.2023.
+//
+
+import Foundation
+
+struct AuthorNetworkModel: Codable {
+    let name: String
+    let id: String
+}
