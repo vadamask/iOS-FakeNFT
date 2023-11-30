7c43ae0ca6f33e01e8c8256231a53d522be5dcb6
Anna Goncharova <bkljn4g@gmail.com>
2023-11-30 05:55:01 +0300

    fix code after review

--- FakeNFT/Foundation/DateFormatters+Presets.swift
+++ FakeNFT/Foundation/DateFormatters+Presets.swift
@@ -2,5 +2,5 @@ import Foundation
 
 extension DateFormatter {
     // We use static var because creating a dateFormatter is an expensive operation and we should do it once
-    static var defaultDateFormatter: ISO8601DateFormatter = .init()
+    static var defaultDateFormatter: ISO8601DateFormatter = ISO8601DateFormatter()
 }
